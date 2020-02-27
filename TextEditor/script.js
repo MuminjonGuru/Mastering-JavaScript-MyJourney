45933a0a9c7dd48afc767b70d66df0ffa25ce96f
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-02-28 04:29:26 +0500

    Create script.js

---
+++ TextEditor/script.js
@@ -0,0 +1,34 @@
+function updateText(){
+  let text = document.getElementById("text-input").value;
+  document.getElementById("text-output").innerText = text;
+}
+
+function makeBold(elem){
+  elem.classList.toggle('active');
+  document.getElementById("text-output").classList.toggle("bold");
+}
+
+function makeItalic(elem){
+  elem.classList.toggle('active');
+  document.getElementById("text-output").classList.toggle("italic");
+}
+
+function makeUnderline(elem){
+    elem.classList.toggle("active");
+    let formattedText = document.getElementById("text-output");
+    if(formattedText.classList.contains("underline")) {
+      formattedText.classList.remove("underline");
+    } else {
+      formattedText.classList.add("underline");
+    }
+}
+
+function alignText(elem, alignType){
+  elem.classList.toggle("active");
+  document.getElementById('text-output').style.textAlign = alignType;
+  let buttonsList = document.getElementsByClassName("align");
+  for(let i = 0; i < buttonsList.length; i++) {
+    buttonsList[i].classList.remove("active");
+  }
+  elem.classList.add("active");
+}
