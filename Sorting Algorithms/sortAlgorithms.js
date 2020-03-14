a8d2c7003364cc7daeab18129b9c8f51749e4981
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-03-14 18:59:47 +0500

    insertion sort

--- Sorting Algorithms/sortAlgorithms.js
+++ Sorting Algorithms/sortAlgorithms.js
@@ -1,3 +1,21 @@
+// insertion sort
+var insertionSort = nums => {
+  for(let i = 1; i < nums.length; i++) {
+    for(let j = 0; j < 1; j++) {
+      
+      console.log(nums);
+      
+      if(nums[i] < nums[j]) {
+        let spliced = nums.splice(i, 1);
+        nums.splice(j, 0, spliced[0]);
+      }
+      
+    }
+  }
+};
+
+
+
 // don't use bubble sort
 
 const bubbleSort = (nums) => {
