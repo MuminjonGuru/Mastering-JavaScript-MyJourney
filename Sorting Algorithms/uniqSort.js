507b42746b21908f08f53a31809bb8cd94a5fdfd
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-03-21 01:11:48 +0500

    linear unique sort

--- Sorting Algorithms/uniqSort.js
+++ Sorting Algorithms/uniqSort.js
@@ -1,6 +1,21 @@
-const uniqSort = function(arr) {
+const uniqSort0 = function(arr) {
   const breadcrumbs = {};
   return arr.sort((a, b) => a - b);
 }
+
+// Linear version
+const uniqSort = function(arr) {
+  const breadcrumbs = {};
+  const result = [];
+
+  for(let i=0; i < arr.length; i++) {
+    if(!breadcrumbs[arr[i]]) {
+      result.push(arr[i]);
+      breadcrumbs[arr[i]] = true;
+    }
+  }
+  return result.sort((a, b) => a - b);
+}
+
 
 uniqSort([4,1,6,2,0]);
