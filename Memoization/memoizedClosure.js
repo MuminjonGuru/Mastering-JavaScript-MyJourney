78c6fb02ec8ece20abff528ca50f422e82064ea2
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-03-21 01:36:01 +0500

    Update memoizedClosure.js

--- Memoization/memoizedClosure.js
+++ Memoization/memoizedClosure.js
@@ -1,3 +1,20 @@
+const times10 = (n) => {return n * 10};
+
+const cache = {};
+
+const memoTimes10 = (n) => {
+  if(n in cache) {
+    console.log('Getting from cache: ', );
+    return cache[n];
+  } else {
+    console.log('Calculating result');
+    let result = times10(n);
+    cache[n] = result;
+    return result;
+  }
+};
+
+
 const memoizedClosure = () => {
   let cache = {};
 	return (n) => {
