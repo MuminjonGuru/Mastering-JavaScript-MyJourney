354f865423c4a30218ae7ccd3dfe459b7acca9c0
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-05-17 02:21:08 +0500

    str output sty.e

---
+++ JS Foundations - Full/stringOutputStyles.js
@@ -0,0 +1,17 @@
+var testStr = "Test?";
+
+var gap = "Salom UY! \"Salom\"";
+gap = 'Salom UY! \"Salom\"';
+
+console.log(gap);
+
+/*
+\'  single quote
+\"  double quote
+\\  backslash
+\n  newline
+\r  carriage return
+\t  tab
+\b  backspace
+\f  form feed
+*/
