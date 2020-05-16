661d5f3d903e96cbf1998b4d0714b8a529f770f9
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-05-17 01:37:56 +0500

    varlet

---
+++ JS Foundations - Full/variables.js
@@ -0,0 +1,19 @@
+/* Variables */
+var myName; // common style
+myName = "Guru";
+
+var state = "Khorezm";
+
+console.log(state);
+
+state = "Tashkent";
+
+console.log(state);
+
+// 0-9, a-z, A-z
+
+var basicDeclaring = "repl.it";
+
+
+// let - the purpose of let statements only to free up memory when not needed in a certain block
+// let variables are not initialized until their definition is evaluated.
