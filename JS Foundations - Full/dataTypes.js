43118f0b68a294ae03812d86b4a400d0b3b6af08
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-05-17 02:13:28 +0500

    7 DT

---
+++ JS Foundations - Full/dataTypes.js
@@ -0,0 +1,42 @@
+// 7 different data types
+
+// Boolean
+var data = true;
+
+if(data) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// null
+
+var n = null;
+console.log(n);
+
+// undefined
+
+var a;
+console.log(a + 1);
+
+// Number not Integer
+
+var num = 3.1415;
+var mun = 14;
+console.log(num + mun);
+
+// String
+
+var name = "Guru";
+console.log(name);
+
+// Symbol
+
+var syml = Symbol("foo");
+console.log(syml);
+
+// Object
+
+var yourObj = new Object();
+yourObj.execute = "Exec.";
+console.log(yourObj.execute);
