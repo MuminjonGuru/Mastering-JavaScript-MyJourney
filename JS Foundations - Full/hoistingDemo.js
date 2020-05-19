686fe32ab493dd31722a78444561957f928fba7f
Abduraimov Muminjon <muminjonguru@outlook.com>
2020-05-19 20:17:53 +0500

    hoisting in JS

---
+++ JS Foundations - Full/hoistingDemo.js
@@ -0,0 +1,10 @@
+// in JS, variables can be declared after it has been utilized
+x = 5;  // assign a value to a variables 
+
+console.log(x);
+
+var x;  // now declare
+////////////////////////Another Example//////////////////////
+var x;
+x = 5;
+console.log(x);
