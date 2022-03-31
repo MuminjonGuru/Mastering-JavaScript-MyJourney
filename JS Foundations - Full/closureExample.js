acee16be9f1b1bab23106bd69e37d80f904c5821
Abduraimov Muminjon <muminjonguru@outlook.com>
2022-03-31 12:54:20 +0500

    updt

--- JS Foundations - Full/closureExample.js
+++ JS Foundations - Full/closureExample.js
@@ -8,3 +8,23 @@ function makeFunc() {
 
 var myFunc = makeFunc();
 myFunc();
+//////////////////////////////////
+
+const promise1 =  new Promise((resolve,reject) => {
+  resolve("my first value");
+});
+const promise2 =  new Promise((resolve,reject) => {
+  reject(Error("oooops error"));
+});
+
+// one of the two promises will fail, but `.all` will return only a rejection.
+Promise
+  .all([promise1, promise2])
+  .then(data => {
+    const[promise1data, promise2data] = data;
+    console.log(promise1data, promise2data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  // Error: oooops error
